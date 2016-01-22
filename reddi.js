fa16a8104772b3d6738879a2606189dd968a564c
Nick Sandonato <nick.sandonato@gmail.com>
2016-01-22 18:50:15 -0500

    Initial contributions for source and build

---
+++ reddi.js
@@ -0,0 +1,22 @@
+const doc = window.document;
+const dcl = 'DOMContentLoaded';
+const cbs = [];
+
+doc.addEventListener(dcl, function handler() {
+	doc.removeEventListener(dcl, handler);
+	cbs.forEach(cb => {
+		cb();
+	});
+}, false);
+
+export default function reddi(cb) {
+	// interactive/complete means it's ready
+	if (/^i|^c/.test(doc.readyState)) {
+		cb();	
+	}
+	else {
+		// Otherwise, queue up the callbacks
+		cbs.push(cb);
+	}
+
+};
